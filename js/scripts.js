7a84f957e941a783fcfc87028a506dd98b0cbffe
Hansen Hansen and Marcus Parmentier <hansen@>
2017-09-21 10:04:17 -0700

    Add the second spec and add split in the user logic.

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,27 @@ var romanOne = function(number) {
   return value;
 }
 
+var romanTwo = function(number) {
+  var value = "";
+  if(number >= 5){
+    value += "L"
+    number = number - 5;
+    if(number > 0) {
+      for(i = 0; i < number; i++) {
+        value += "X";
+    }
+  }
+}
+  else if(number < 4) {
+    for(i = 0; i < number; i++) {
+      value += "X";
+    }
+
+  }
+  return value;
+}
+
+
 
 
 
@@ -26,8 +47,21 @@ var romanOne = function(number) {
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var input = ($("input#input").val());
-    var result = romanOne(input);
+
+    var input = parseInt($("input#input").val());
+    var digits = (""+input).split("");
+    var splitInput = [];
+    digits.forEach(function(digit) {
+      splitInput.push(digit);
+    });
+
+    splitInput.reverse();
+
+    var result = romanTwo(splitInput[1])+romanOne(splitInput[0]);
+    // for(i = 0; i < splitInput.length; i++){
+    //   var result = romanOne(splitInput[i]);
+    // }
+    // var result = romanOne(splitI);
     $("#result").text(result);
   });
 });
