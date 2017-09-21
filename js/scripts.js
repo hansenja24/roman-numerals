f354c6def048420ee1fe6d79df13e41bd262bccb
Hansen Hansen and Marcus Parmentier <hansen@>
2017-09-21 13:13:18 -0700

    simplfy the code by using for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,105 @@
 //********************Business Logic************************
 
-var romanOne = function(number) {
+var valueFifth = ["V","L","D"];
+var valueSingle  = ["I","X","C","M"];
 
-  var value = "";
-  if(number >= 5 && number < 9){
-    value += "V"
-    number = number - 5;
-    if(number > 0) {
-      for(i = 0; i < number; i++) {
-        value += "I";
+
+var roman = function(number) {
+  var value = [];
+  var mix = "";
+  debugger;
+  for(i = 0; i < length; i++){
+    if(number[i] >= 5 && number[i] < 9){
+      mix = (valueFifth[i]);
+      number[i] = number[i] - 5;
+      if(number[i] > 0) {
+        for(j = 0; j < number[i]; j++) {
+          mix += (valueSingle[i]);
+        }
+      }
     }
-  }
-}
-  else if(number < 4) {
-    for(i = 0; i < number; i++) {
-      value += "I";
+    else if(number[i] < 4) {
+      for(j = 0; j < number[i]; j++) {
+        mix = (valueSingle[i]);
+      }
     }
-  }
-  else if(number == 4){
-    value = "IV";
-  }
-  else if(number == 9){
-    value = "IX";
+    else if(number[i] == 4){
+      mix = (valueSingle[i] + valueFifth[i]);
+    }
+    else if(number[i] == 9){
+      mix = (valueSingle[i] + valueSingle[i+1]);
+    }
+    value.push(mix);
   }
   return value;
 }
 
-var romanTwo = function(number) {
 
-  var value = "";
-  if(number >= 5 && number < 9){
-    value += "L"
-    number = number - 5;
-    if(number > 0) {
-      for(i = 0; i < number; i++) {
-        value += "X";
-    }
-  }
-}
-  else if(number < 4) {
-    for(i = 0; i < number; i++) {
-      value += "X";
-    }
-
-  }
-  else if(number == 4){
-    value = "XL";
-  }
-  else if(number == 9){
-    value = "XC";
-  }
-  return value;
-}
-
-var romanThree = function(number) {
-    debugger;
-  var value = "";
-  if(number >= 5 && number < 9){
-    value += "D"
-    number = number - 5;
-    if(number > 0) {
-      for(i = 0; i < number; i++) {
-        value += "C";
-    }
-  }
-}
-  else if(number < 4) {
-    for(i = 0; i < number; i++) {
-      value += "C";
-    }
-
-  }
-  else if(number == 4){
-    value = "CD";
-  }
-  else if(number == 9){
-    value = "CM";
-  }
-  return value;
-}
-
-var romanFour = function(number) {
-  var value = "";
-
-  if(number < 4) {
-    for(i = 0; i < number; i++) {
-      value += "M";
-    }
-
-  }
-  return value;
-}
+// var romanTwo = function(number) {
+//
+//   var value = "";
+//   if(number >= 5 && number < 9){
+//     value += "L"
+//     number = number - 5;
+//     if(number > 0) {
+//       for(i = 0; i < number; i++) {
+//         value += "X";
+//     }
+//   }
+// }
+//   else if(number < 4) {
+//     for(i = 0; i < number; i++) {
+//       value += "X";
+//     }
+//
+//   }
+//   else if(number == 4){
+//     value = "XL";
+//   }
+//   else if(number == 9){
+//     value = "XC";
+//   }
+//   return value;
+// }
+//
+// var romanThree = function(number) {
+//     debugger;
+//   var value = "";
+//   if(number >= 5 && number < 9){
+//     value += "D"
+//     number = number - 5;
+//     if(number > 0) {
+//       for(i = 0; i < number; i++) {
+//         value += "C";
+//     }
+//   }
+// }
+//   else if(number < 4) {
+//     for(i = 0; i < number; i++) {
+//       value += "C";
+//     }
+//
+//   }
+//   else if(number == 4){
+//     value = "CD";
+//   }
+//   else if(number == 9){
+//     value = "CM";
+//   }
+//   return value;
+// }
+//
+// var romanFour = function(number) {
+//   var value = "";
+//
+//   if(number < 4) {
+//     for(i = 0; i < number; i++) {
+//       value += "M";
+//     }
+//
+//   }
+//   return value;
+// }
 
 
 
@@ -101,23 +110,25 @@ $(document).ready(function() {
 
     var input = $("input#input").val();
     var digits = input.split("");
-    var splitInput = [];
+    var splitInputs = [];
+    var result = "";
     digits.forEach(function(digit) {
-      splitInput.push(digit);
+      splitInputs.push(digit);
     });
 
-    splitInput.reverse();
+    splitInputs.reverse();
+    length = splitInputs.length;
 
-    if(splitInput[3] >= 4){
+    if(splitInputs[3] >= 4){  //puts or put?????????
       alert("Please enter a number less than 4,000");
     } else {
-      var result = romanFour(splitInput[3]) + romanThree(splitInput[2]) + romanTwo(splitInput[1]) + romanOne(splitInput[0]);
-    }
+      // var result = romanFour(splitInput[3]) + romanThree(splitInput[2]) + romanTwo(splitInput[1]) + romanOne(splitInput[0]);
+        result = roman(splitInputs); //put or puts?????????
+      };
+      result.reverse();
+      result = result.join('');
 
-    // for(i = 0; i < splitInput.length; i++){
-    //   var result = romanOne(splitInput[i]);
-    // }
-    // var result = romanOne(splitI);
+
     $("#result").text(result);
   });
 });
