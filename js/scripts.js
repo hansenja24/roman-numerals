79f5f22fc26cf5d1a818e0dcdb19e368a06143e6
Hansen Hansen and Marcus Parmentier <hansen@>
2017-09-21 10:11:08 -0700

    Add the third spec and fixes the split functiion

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,45 @@ var romanTwo = function(number) {
   return value;
 }
 
+var romanThree = function(number) {
+  var value = "";
+  if(number >= 5){
+    value += "D"
+    number = number - 5;
+    if(number > 0) {
+      for(i = 0; i < number; i++) {
+        value += "C";
+    }
+  }
+}
+  else if(number < 4) {
+    for(i = 0; i < number; i++) {
+      value += "C";
+    }
 
+  }
+  return value;
+}
+
+// var romanFour = function(number) {
+//   var value = "";
+//   if(number >= 5){
+//     value += "V"
+//     number = number - 5;
+//     if(number > 0) {
+//       for(i = 0; i < number; i++) {
+//         value += "I";
+//     }
+//   }
+// }
+//   else if(number < 4) {
+//     for(i = 0; i < number; i++) {
+//       value += "I";
+//     }
+//
+//   }
+//   return value;
+// }
 
 
 
@@ -48,8 +86,8 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
 
-    var input = parseInt($("input#input").val());
-    var digits = (""+input).split("");
+    var input = $("input#input").val();
+    var digits = input.split("");
     var splitInput = [];
     digits.forEach(function(digit) {
       splitInput.push(digit);
@@ -57,7 +95,7 @@ $(document).ready(function() {
 
     splitInput.reverse();
 
-    var result = romanTwo(splitInput[1])+romanOne(splitInput[0]);
+    var result = romanThree(splitInput[2]) + romanTwo(splitInput[1]) + romanOne(splitInput[0]);
     // for(i = 0; i < splitInput.length; i++){
     //   var result = romanOne(splitInput[i]);
     // }
